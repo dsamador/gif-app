c023f6364922597f8546cf73f6b1c7fc1cd4adbb
David Sebastian Amador Pedrozo <dsamador@misena.edu.co>
2021-08-09 20:33:55 -0500

    Se valida el input

---
+++ src/GifExpertApp.js
@@ -0,0 +1,23 @@
+import {useState} from 'react'
+import AddCategory from './components/AddCategory'
+
+const GifExpertApp = () => {    
+    const [categories, setCategories] = useState(['flag', 'ship', 'plane'])    
+
+    return ( 
+        <>
+            <h2>GifExpertApp</h2>
+            <AddCategory setCategories={setCategories} />
+            <hr/>
+            <ol> 
+                {
+                    categories.map(category =>{
+                        return <li key={category}>{category}</li>
+                    })
+                }
+            </ol>
+        </>
+     );
+}
+ 
+export default GifExpertApp;
