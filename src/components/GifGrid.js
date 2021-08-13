c7537ac61fc9d98abedd64432ec307e471018350
David Sebastian Amador Pedrozo <dsamador@misena.edu.co>
2021-08-13 06:38:01 -0500

    Empezamos a presentar las imagenes

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,16 +1,18 @@
 import React, {useState, useEffect} from 'react'
+import { GifGridItem } from './GifGridItem'
 
 
 export const GifGrid = ({category}) => {
 
-    const [count, setCount] = useState(0)
+    const [images, setImages] = useState([])
+    
     //Use effect ejecuta una función cuando el componente es renderizado por primera vez
     useEffect(()=>{
         getGifs()
     }, [])
 
     const getGifs = async()=>{
-        const url = 'https://api.giphy.com/v1/gifs/search?q=phineas&limit=1&api_key=CrEAOue8VpcjPYg0t7FbgEE3olJ4Hato';        
+        const url = 'https://api.giphy.com/v1/gifs/search?q=phineas&limit=10&api_key=CrEAOue8VpcjPYg0t7FbgEE3olJ4Hato';        
         const resp = await fetch(url)
         const {data} = await resp.json()
 
@@ -21,15 +23,18 @@ export const GifGrid = ({category}) => {
                 url: img.images?.downsized_medium.url
             }
         })
-
-        console.log(gifs)
+        
+        setImages(gifs)
     }    
 
     return (
         <div>
-            <h3>{category}</h3>
-            <h3>{count}</h3>
-            <button onClick={()=>setCount(count + 1)}>+</button>
+            <h3>{category}</h3>             
+            {
+                images.map(img=>(
+                    <GifGridItem key={img.id} {...img}/>
+                ))
+            }            
         </div>
     )
 }
---
+++ src/components/GifGridItem.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const GifGridItem = ({id,title,url}) => {
+    return (
+        <div>
+            <img src={url} alt={title} />
+        </div>
+    )
+}
