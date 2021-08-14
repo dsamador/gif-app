72b5777ba28f46a89641a2e44357cb31af772e2c
David Sebastian Amador Pedrozo <dsamador@misena.edu.co>
2021-08-13 19:24:57 -0500

    Uso de className

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -3,7 +3,7 @@ import AddCategory from './components/AddCategory'
 import { GifGrid } from './components/GifGrid';
 
 const GifExpertApp = () => {    
-    const [categories, setCategories] = useState(['flag'])    
+    const [categories, setCategories] = useState([''])    
 
     return ( 
         <>
--- src/components/GifGridItem.js
+++ src/components/GifGridItem.js
@@ -2,8 +2,9 @@ import React from 'react'
 
 export const GifGridItem = ({id,title,url}) => {
     return (
-        <div>
+        <div className="card">
             <img src={url} alt={title} />
+            <p>{title}</p>
         </div>
     )
 }
